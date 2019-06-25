071eab7ef8a7a9aa68d8371c5e61b73b12154f36
richy <richy_riched@yahoo.com>
2019-06-25 16:41:19 -0700

    Updating Java Docs

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -2,12 +2,21 @@ package Game;
 import java.util.ArrayList;
 import Cards.Card;
 
-
+/**
+ * This is the player class that we need for our Illuminati game. Represents the individual player
+ *
+ * @author ARK
+ * @since 25 June 2019
+ *
+ */
 public class Player {
     //instance variables
     private String name ;
     private ArrayList<Card> cards;
 
+    /**
+     * This is the basic constructor for our player class.
+     */
     public Player(){
         name = "Default";
         cards = new ArrayList<Card>() ;
