d30e3f8ed3af0ac5a434fe74b1f204d2c7562c8b
richy <richy_riched@yahoo.com>
2019-06-25 16:35:03 -0700

    Updated Player Class

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -6,5 +6,12 @@ import Cards.Card;
 public class Player {
     //instance variables
     private String name ;
-    private ArrayList<Card> Cards;
+    private ArrayList<Card> cards;
+
+    public Player(){
+        name = "Default";
+        cards = new ArrayList<Card>() ;
+    }
+
 }
+
