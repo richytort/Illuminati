901f81001d8a729b9ee50d2340b439804061f515
richy <richy_riched@yahoo.com>
2019-06-25 21:46:46 -0700

    Main menu changes

--- src/Tester/MainMenuTester.java
+++ src/Tester/MainMenuTester.java
@@ -7,7 +7,10 @@ import javax.swing.*;
 public class MainMenuTester {
     public static void main(String[] args) {
         MainMenuFrame frame = new MainMenuFrame();
+        frame.setTitle("MainMenu");
+
         frame.setContentPane(new MainMenuFrame().getMainMenuPanel() );
+        frame.setSize(450, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
