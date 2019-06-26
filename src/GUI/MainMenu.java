ab1f263ba4e12d20bd77240d1a6c9acb1cb11645
richy <richy_riched@yahoo.com>
2019-06-25 19:59:46 -0700

    MainMenu Stuff

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -24,11 +25,41 @@ public class MainMenu extends JFrame{
        JPanel p = new JPanel() ;
        startButton = new JButton("Start") ;
        createPlayerButton = new JButton("Create Player");
+
+        ActionListener startListener = new StartListener() ;
+        startButton.addActionListener( startListener);
+
+
        p.add(createPlayerButton);
        p.add(startButton);
 
 
        this.add(p);
+
+
+
+    }
+
+    class StartListener implements ActionListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            //open a different frame with drop menus for the different coffee options
+            p.add(scrollPane); //added this text field. MAYBE PLACE IT SOMEWHEREELSE
+
+            CoffeePurchaseFrame coffeeFrame = new CoffeePurchaseFrame() ;
+
+            coffeeFrame.setVisible(true);
+
+            coffeeFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+
+            ///////////////////maybe i can place these on the coffee purchase frame after they select done
+            p.add(doneButton);
+            p.revalidate();
+        }
+
 
     }
 }
